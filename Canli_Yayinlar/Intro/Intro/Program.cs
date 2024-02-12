cc21f8c9982558c930870a0eeda5ed0370688dbd
MuratEltturkey <murataltunkaya7@gmail.com>
2024-02-12 15:48:27 +0300

    Edit

---
+++ Canli_Yayinlar/Intro/Intro/Program.cs
@@ -0,0 +1,41 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+//variables --> camelCase
+bool isAuthenticated = false; 
+Console.WriteLine(message1);
+
+//condition
+
+if (isAuthenticated)
+{
+    Console.WriteLine("Button => Hoşgeldin Engin");
+}
+else
+{
+    Console.WriteLine("Button => Sisteme giriş yap");
+}
+
+string[] loans = { "Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5", "Kredi6" };
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length ; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+Console.WriteLine("kod bitti");
+
---
+++ Task_1/MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();
+            
+
+
+            Console.ReadLine();
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000;
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.18);
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
