0107923ce715a9c86e5b6204ea40ef8366d96484
MuratEltturkey <murataltunkaya7@gmail.com>
2024-02-12 15:53:03 +0300

    Edit

--- Task_1/MaskeTakip/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Task_1/MaskeTakip/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc21f8c9982558c930870a0eeda5ed0370688dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
